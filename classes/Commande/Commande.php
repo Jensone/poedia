d6b3386f4d08bc3e2b8cbbe06981eb85b5d27475
Eva <eva.caroline.szeremeta@gmail.com>
2023-06-02 16:49:45 +0200

    Ajout propriétés et méthodes de la classe Commande

--- classes/Commande/Commande.php
+++ classes/Commande/Commande.php
@@ -6,11 +6,55 @@
 
 namespace App;
 
+use App\Database;
+
 class Commande
 {
     // Propriétés
+    private $id;
+    private $client;
+    private $livre;
+    private $dateDebut;
+    private $dateRetour;
+    private $statut;
 
-    // Méthodes
+    // Constructeur
+    public function __construct(int $id, string $client, string $livre, string $debutDate, string $dateRetour, bool $statut)
 
+    {
+        $this->id = $id;
+        $this->client = $client;
+        $this->livre = $livre;
+        $this->dateDebut = $dateDebut;
+        $this->dateRetour = $dateRetour;
+        $this->statut = $statut;
+    }
+
+    // Méthodes (Get et Setter)
+
+    public function getClient(): string
+    {
+        return $this->client;
+    }
+
+    public function getLivre(): string
+    {
+        return $this->livre;
+    }
+
+    public function getDateDebut(): int
+    {
+        return $this->dateDebut;
+    }
+
+    public function getDateRetour(): int
+    {
+        return $this->dateRetour;
+    }
+
+    public function getStatut(): bool
+    {
+        return $this->statut;
+    }
     
 }
