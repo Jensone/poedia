f9c1fec9cd85f954a5b3650ad42f0eab0468c90f
Marcelin <marcelintingougoui3@gmail.com>
2023-06-02 16:49:25 +0200

    ajout des proprietes et des methodes dans la classe auteur

--- classes/Auteur/Auteur.php
+++ classes/Auteur/Auteur.php
@@ -9,7 +9,72 @@
 
  class Auteur{
     //Propiétés
+    private $id;
+    private $nom;
+    private $prenom;
+    private $dateNaissance;
+    private $nationalite;
+    private $biographi; 
 
-
-    //Methodes
+     public function __construct(int $Id, string $nom,string $prenom, string $dateNaissance, string $nationalite,string $biographi )
+    {
+        $this->nom =$nom;
+        $this->prenom =$prenom;
+        $this->dateNaissance =$dateNaissance;
+        $this->nationalite =$nationalite;
+        $this->biographi =$biographi;
+    }
+    //Getters
+    
+    public function getNom():string
+    {
+       return $this -> nom;
+    } 
+    public function getPrenom():string
+    {
+      return $this -> prenom;
+        
+    }
+    public function getdateNaissance():string
+    {
+      return $this -> dateNaissance;
+        
+    }
+    public function getnationalite():string
+    {
+      return $this -> nationalite;
+        
+    }
+    public function getbiographi():string
+    {
+      return $this -> biographi;
+        
+    }
+    //setters
+    public function setNom($nom):void
+    {
+        $this ->nom=$nom;
+    } 
+    public function setPrenom($prenom):void
+    {
+       $this ->prenom=$prenom;
+        
+    }
+    public function setdateNaissance($dateNaissance):void
+    {
+       $this ->dateNaissance=$dateNaissance;
+        
+    }
+    public function setnationalite($nationalite):void
+    {
+       $this ->nationalite=$nationalite;
+        
+    }
+    public function setbiographi($biographi):void
+    {
+       $this ->biographi=$biographi;
+        
+    }
  }
+
+ 
