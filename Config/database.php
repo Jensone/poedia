5a6be68e47981d28f31ec0a8c136e2c17cb4c314
Marcelin <marcelintingougoui3@gmail.com>
2023-06-02 16:48:37 +0200

    completion des élément de la base de donne avec des request

--- Config/database.php
+++ Config/database.php
@@ -8,19 +8,52 @@
  * @subpackage Database
  */
 
-namespace BiblioApp;
+namespace App;
 
 class Database
 {
-    // Méthodes (fonctions)
+    // connexion
     public static function connect(): \PDO
     {
+        $host=getenv('HOST');
+        $dbname=getenv('DBNAME');
+        $user=getenv('USER');
+        $password=getenv('PASSWORD');
         try {
-            $pdo = new \PDO('mysql:host=localhost;dbname=bibl', 'root', '',);
+            $pdo = new \PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8'.$user,'',);
             $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             return $pdo;
         } catch (\PDOException $e) {
             echo 'Erreur de connexion : ' . $e->getMessage();
         }
     }
+
+    // Recupérer un item
+    public static function Afficherun(string $table, int $id): Array{
+      $pdo=self::connect();
+      $query=$pdo->prepare("SELECT * FROM $table WHERE id=:id");
+      $query->execute(array('id'=>$id));
+      $item=$query->fetch(\PDO::FETCH_ASSOC);
+      return $item;
+
+    }
+    //Recupérer des item
+    public static function Affichertout(string $table, int $id):Array{
+        $pdo=self::connect();
+        $query=$pdo->prepare("SELECT * FROM $table WHERE id=:id");
+        $query->execute(array('id'=>$id));
+        $item=$query->fetch(\PDO::FETCH_ASSOC);
+      return $item;
+  
+      }
+    // Récupere un item par une valeur
+    public static function Afficherparvaleur( string $table, string $valeur,){
+        $pdo=self::connect();
+        $query=$pdo->prepare("SELECT * FROM $table WHERE (CONVERT($valeur USING utf8)lIKE %$valeur% ");
+        $query->execute();
+        $query->fetch(\PDO::FETCH_ASSOC);
+        return $query;
+  
+      }
+   
 }
