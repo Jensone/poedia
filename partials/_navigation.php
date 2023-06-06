de6ac559906e119ccce66c977963799978ac16a3
Jensone <hello@jensone.com>
2023-06-06 16:23:17 +0200

    🎨 style(_navigation.php): update navigation links and buttons text to french

--- partials/_navigation.php
+++ partials/_navigation.php
@@ -29,7 +29,7 @@
     <div class="d-none d-lg-block">
       <div class="bg-white bg-opacity-25 rounded-3">
         <button class="btn btn-transparent p-4 rounded-3" type="button">
-          <span class="fs-20 text-light text-uppercase mb-0" style="letter-spacing: 1px;">Start Free Trial</span>
+          <span class="fs-20 text-light text-uppercase mb-0" style="letter-spacing: 1px;">Coonexion</span>
         </button>
       </div>
     </div>
@@ -61,35 +61,32 @@
     <div class="py-16">
       <ul class="nav flex-column">
         <li class="nav-item mb-12">
-          <a class="nav-link d-inline-block p-0" href="#">
-            <h3 class="fs-17 fw-medium text-secondary mb-0">Features</h3>
+          <a class="nav-link d-inline-block p-0" href="/livres.php">
+            <h3 class="fs-17 fw-medium text-secondary mb-0">Bibliothèque</h3>
           </a>
         </li>
         <li class="nav-item mb-12">
-          <a class="nav-link d-inline-block p-0" href="#">
-            <h3 class="fs-17 fw-medium text-secondary mb-0">Solutions</h3>
+          <a class="nav-link d-inline-block p-0" href="/commandes.php">
+            <h3 class="fs-17 fw-medium text-secondary mb-0">Commandes</h3>
           </a>
         </li>
         <li class="nav-item mb-12">
-          <a class="nav-link d-inline-block p-0" href="#">
-            <h3 class="fs-17 fw-medium text-secondary mb-0">Resources</h3>
+          <a class="nav-link d-inline-block p-0" href="/clients.php">
+            <h3 class="fs-17 fw-medium text-secondary mb-0">Clients</h3>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link d-inline-block p-0" href="#">
-            <h3 class="fs-17 fw-medium text-secondary mb-0">Pricing</h3>
+          <a class="nav-link d-inline-block p-0" href="/auteurs.php">
+            <h3 class="fs-17 fw-medium text-secondary mb-0">Auteurs</h3>
           </a>
         </li>
       </ul>
     </div>
     <div>
       <button class="btn btn-light px-4 py-3 w-100 mb-5 rounded-1" type="button">
-        <span class="fs-17 fw-medium text-secondary mb-0">Login</span>
+        <span class="fs-17 fw-medium text-secondary mb-0">Connexion</span>
       </button>
       <div class="d-inline-block w-100 rounded-3" style="padding:2px; background: linear-gradient(90deg, rgba(108,213,246,1 ) 0%, rgba(248,157,92,1) 50%, rgba(91,106,240,1) 100%)">
-        <button class="btn btn-light px-4 py-3 w-100 rounded-1" type="button">
-          <span class="fs-17 fw-medium text-secondary mb-0 border-0">Start Free Trial</span>
-        </button>
       </div>
     </div>
   </nav>
