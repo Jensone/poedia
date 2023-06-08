866bde7fee5e65c81ad67ab03aa6479b111d6d74
Marcelin <marcelintingougoui3@gmail.com>
2023-06-08 16:12:43 +0200

    ➕ ajout du lien css dans le head

--- partials/_head.php
+++ partials/_head.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Outfit:wght@500;600;700&display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap">
     <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style/main.css">
+
     <link rel="icon" type="image/png" sizes="32x32" href="shuffle-for-bootstrap.png">
 </head>
 <body>
