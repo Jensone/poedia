5dd5d0b5a72a4532e622bd2d6605b6c2afe0f248
Jensone <hello@jensone.com>
2023-06-21 14:49:24 +0200

    ➕ Création de l'entité Admin (make:user) - Mise en place d'un Login, Logout, Register - Personnalisation des routes : homepage, contact, login, logout - Personnalisation du template de vérification de mail

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ContactController extends AbstractController
+class PageController extends AbstractController
 {
-    #[Route('/contact', name: 'contact')]
+    #[Route('/', name: 'homepage')]
     public function index(): Response
     {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
         ]);
     }
 }
