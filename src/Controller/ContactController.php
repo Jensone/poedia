50a313f9df79b2986353bb8134c157af5e79b18c
Aurélien <107919117+Issei1801@users.noreply.github.com>
2023-06-22 11:33:14 +0200

    🏷️ Création des input dans contactType + route

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,47 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ContactType;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(): Response
+    public function index(
+        Request $request,
+        MailerInterface $mailer
+    ): Response
     {
+        $form = $this->createForm(ContactType::class);
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $value = $form->getData();
+
+            $email = (new Email())
+            ->from($value['E-mail'])
+            ->to($value['Admin'])
+            ->priority(Email::PRIORITY_HIGH)
+            ->subject($value['objet'])
+            ->text($value['message'])
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+
+        $this->addFlash('success', 'Votre message a bien été envoyé');
+        $this->addFlash('error', 'Votre message n\'a pas pu être envoyé');
+        }
+
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,14 +4,107 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+
+
+        
+        ->add('firstName', TextType::class, [
+            'label' => 'Prénom',
+            'attr' => ['placeholder' => 'Votre prénom'],
+            'required' => 'true',
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Auriez-vous oublié votre prénom ?'
+                ]),
+                new Length([
+                    'min' => 2,
+                    'minMessage' => 'Ecrivez un minimum de {{ limit }} caractères',
+                    'max' => 30,
+                    'minMessage' => 'Vous avez dépassé la limte de {{ limit }} caractères'
+                ]),
+                new Regex([
+                    'pattern' => '/^[a-zA-Z]+$/',
+                    'message' => 'Vous avez écrit des carctères non valides'
+                    ])
+            ]
+        ])
+
+        
+        ->add('lastName', TextType::class, [
+            'label' => 'Nom',
+            'attr' => ['placeholder' => 'Votre nom'],
+            'required' => 'true',
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Auriez-vous oublié votre nom ?'
+                ]),
+                new Length([
+                    'min' => 2,
+                    'minMessage' => 'Ecrivez un minimum de {{ limit }} caractères',
+                    'max' => 30,
+                    'minMessage' => 'Vous avez dépassé la limte de {{ limit }} caractères'
+                ]),
+                new Regex([
+                    'pattern' => '/^[a-zA-Z]+$/',
+                    'message' => 'Vous avez écrit des carctères non valides'
+                    ])
+            ]
+        ])
+
+        
+        ->add('E-mail', EmailType::class, [
+            'label' => 'E-mail',
+            'attr' => ['placeholder' => 'Votre mail'],
+            'required' => 'true',
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Auriez-vous oublié votre nom ?'
+                ]),
+                new Regex([
+                    'pattern' => "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/",
+                    'message' => 'Vous avez écrit des carctères non valides'
+                    ])
+            ]
+        ])
+
+        
+        ->add('telephone', TelType::class, [
+            'label' => 'Numéro de téléphone',
+            'attr' => ['placeholder' => 'Votre numéro de téléphone'],
+        ])
+
+        
+        ->add('objet', TextType::class, [
+            'label' => 'Objet de votre message',
+            'attr' => ['placeholder' => 'Ecrivez l\'objet de votre message'],
+            'required' => 'true'
+        ])
+
+        
+        ->add('message', TextareaType::class, [
+            'label' => 'Message',
+            'attr' => ['placeholder' => 'Ecrivez votre message ici'],
+            'required' => 'true'
+        ])
+
+        
+        ->add('submit', SubmitType::class, [
+            'label' => 'Envoyer'
+        ])
         ;
     }
 
