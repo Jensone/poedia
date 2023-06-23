64e4f08243d5e8cc9d1ea655730d8288764792cf
Jensone <hello@jensone.com>
2023-06-23 12:03:06 +0200

    🚧 Finalisation du formulaire d'ajour d'un client, Ajour de la "Vue" pour le données des Clients en individuel

--- src/Controller/Admin/ClientCrudController.php
+++ src/Controller/Admin/ClientCrudController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Client;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ClientCrudController extends AbstractCrudController
@@ -23,12 +27,26 @@ class ClientCrudController extends AbstractCrudController
             TextField::new('firstname'),
             TextField::new('lastname'),
             EmailField::new('email'),
-            TextField::new('phone'),
-            TextField::new('address'),
-            TextField::new('city'),
-            TextField::new('postal'),
-            TextField::new('country'),
-            
+            TextField::new('phone')->hideOnIndex(),
+            TextField::new('password')->onlyOnForms(),
+            TextField::new('address')->hideOnIndex(),
+            TextField::new('city')->hideOnIndex(),
+            TextField::new('postal')->hideOnIndex(),
+            TextField::new('country')->hideOnIndex(),
+            BooleanField::new(
+                'caution',
+                'Caution payée ?'
+            )
+                ->setHelp(
+                    'Cochez la case si la caution a été payée.'
+                ),
         ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->add(Crud::PAGE_EDIT, Action::SAVE_AND_ADD_ANOTHER);
     }
 }
