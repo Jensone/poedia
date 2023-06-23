833fa6154cff4e02c6cac7df7f0fe5795b7f18c9
Amine <amine.tifour.1973@gmail.com>
2023-06-23 15:10:44 +0200

    🎨 Finalisation du Book-form

--- src/Controller/Admin/BookCrudController.php
+++ src/Controller/Admin/BookCrudController.php
@@ -12,10 +12,12 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
@@ -30,7 +32,6 @@ class BookCrudController extends AbstractCrudController
     public function configureActions(Actions $actions): Actions
 {
     return $actions
-        // ...
         ->add(Crud::PAGE_INDEX, Action::DETAIL)
         ->add(Crud::PAGE_EDIT, Action::SAVE_AND_ADD_ANOTHER)
     ;
@@ -40,11 +41,10 @@ class BookCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-           // IdField::new('id'),
             TextField::new('title'),
-            IntegerField::new('isbn'),
+            TextField::new('isbn'),
             IntegerField::new('pages'),
-            DateTimeField::new('publicationDate'),
+            DateField::new('publicationDate'),
             AssociationField::new('category')
             ->setCrudController(CategoryCrudController::class),
             AssociationField::new('author')
@@ -53,13 +53,13 @@ class BookCrudController extends AbstractCrudController
                 ->setCrudController(EditionCrudController::class),
             AssociationField::new('format')
                 ->setCrudController(FormatCrudController::class),
-            // AssociationField::new('stock')
-            //     ->setCrudController(StockCrudController::class),
+            AssociationField::new('stock')
+                ->setCrudController(StockCrudController::class),
             ImageField::new('cover')
             ->setUploadedFileNamePattern('[slug]-[contenthash].[extension]')
             ->setBasePath('images/book/')
             ->setUploadDir('public/images/book/'),
-            TextEditorField::new('description'),
+            TextareaField::new('description'),
         ];
     }
     
