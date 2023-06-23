38b9cff36f9fd22b02fae0c0e92f54e7d3141e0b
Jensone <hello@jensone.com>
2023-06-23 15:40:09 +0200

    💄 Allègement de la page d'index des livres

--- src/Controller/Admin/BookCrudController.php
+++ src/Controller/Admin/BookCrudController.php
@@ -30,60 +30,61 @@ class BookCrudController extends AbstractCrudController
     }
 
     public function configureActions(Actions $actions): Actions
-{
-    return $actions
-        ->add(Crud::PAGE_INDEX, Action::DETAIL)
-        ->add(Crud::PAGE_EDIT, Action::SAVE_AND_ADD_ANOTHER)
-    ;
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->add(Crud::PAGE_EDIT, Action::SAVE_AND_ADD_ANOTHER);
     }
 
-    
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            TextField::new('title','Titre')
-            ->setHelp('Le titre du livre à ajouter'),
+            TextField::new('title', 'Titre')
+                ->setHelp('Le titre du livre à ajouter'),
 
-            TextField::new('isbn','ISBN')
-            ->setHelp("L'ISBN du livre à ajouter"),
+            TextField::new('isbn', 'ISBN')
+                ->setHelp("L'ISBN du livre à ajouter")
+                ->hideOnIndex(),
 
-            IntegerField::new('pages','Nombre de Pages')
-            ->setHelp('Nombre de pages que contient le livre')
-            ->hideOnIndex(),
+            IntegerField::new('pages', 'Nombre de Pages')
+                ->setHelp('Nombre de pages que contient le livre')
+                ->hideOnIndex(),
 
-            DateField::new('publicationDate','Date de publication')
-            ->setHelp("Date du livre publié par l'éditeur"),
+            DateField::new('publicationDate', 'Date de publication')
+                ->setHelp("Date du livre publié par l'éditeur"),
 
-            AssociationField::new('category','Catégorie')
-            ->setCrudController(CategoryCrudController::class)
-            ->setHelp('La catégorie auquelle appartient le livre'),
+            AssociationField::new('category', 'Catégorie')
+                ->setCrudController(CategoryCrudController::class)
+                ->setHelp('La catégorie auquelle appartient le livre')
+                ->hideOnIndex(),
 
-            AssociationField::new('author','Auteur')
+            AssociationField::new('author', 'Auteur')
                 ->setCrudController(AuthorCrudController::class)
                 ->setHelp("L'auteur du livre"),
 
-            AssociationField::new('edition','Edition')
+            AssociationField::new('edition', 'Edition')
                 ->setCrudController(EditionCrudController::class)
                 ->setHelp("La maison d'édition du livre"),
 
-            AssociationField::new('format','Format')
+            AssociationField::new('format', 'Format')
                 ->setCrudController(FormatCrudController::class)
                 ->setHelp('Le format du livre'),
-
-            AssociationField::new('stock','Stock')
+                
+            AssociationField::new('stock', 'Stock')
                 ->setCrudController(StockCrudController::class)
-                ->setHelp('Combien il en reste en stock'),
+                ->setHelp('Combien il en reste en stock')
+                ->hideOnIndex(),
 
-            ImageField::new('cover','Couverture')
-            ->setUploadedFileNamePattern('[slug]-[contenthash].[extension]')
-            ->setBasePath('images/book/')
-            ->setUploadDir('public/images/book/')
-            ->setHelp('La première de couverture du livre'),
+            ImageField::new('cover', 'Couverture')
+                ->setUploadedFileNamePattern('[slug]-[contenthash].[extension]')
+                ->setBasePath('images/book/')
+                ->setUploadDir('public/images/book/')
+                ->setHelp('La première de couverture du livre'),
 
-            TextareaField::new('description','Description')
-            ->setHelp("Un résumé de l'histoire")
-            ->hideOnIndex(),
+            TextareaField::new('description', 'Description')
+                ->setHelp("Un résumé de l'histoire")
+                ->hideOnIndex(),
         ];
     }
-    
 }
