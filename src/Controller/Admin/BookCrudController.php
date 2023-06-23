48dc076b9d08f23bf9521ba32fcb2ec56cbae60e
Amine <amine.tifour.1973@gmail.com>
2023-06-23 11:17:10 +0200

    🎨 Restructuration du formulaire book-entity

--- src/Controller/Admin/BookCrudController.php
+++ src/Controller/Admin/BookCrudController.php
@@ -3,6 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Book;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class BookCrudController extends AbstractCrudController
@@ -12,14 +19,31 @@ class BookCrudController extends AbstractCrudController
         return Book::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+           // IdField::new('id'),
             TextField::new('title'),
+            IntegerField::new('isbn'),
+            IntegerField::new('pages'),
+            DateTimeField::new('publicationDate'),
+            AssociationField::new('category')
+            ->setCrudController(CategoryCrudController::class),
+            AssociationField::new('author')
+                ->setCrudController(AuthorCrudController::class),
+            AssociationField::new('edition')
+                ->setCrudController(EditionCrudController::class),
+            AssociationField::new('format')
+                ->setCrudController(FormatCrudController::class),
+            AssociationField::new('stock')
+                ->setCrudController(StockCrudController::class),
+            ImageField::new('cover')
+            ->setUploadedFileNamePattern('[slug]-[contenthash].[extension]')
+            ->setBasePath('images/book/')
+            ->setUploadDir('public/images/book/'),
             TextEditorField::new('description'),
         ];
     }
-    */
+    
 }
